269c9f8e0d7c71e95dc0b42732157de65c6c896d
Noah Ulrich <nulrich@geico.com>
2021-05-18 19:25:26 +0000

    commit3

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,1 +1,11 @@
-SELECT ID as CUSTOMER_ID, ORDER_ID, AMOUNT/100 as AMOUNT, STATUS FROM RAW.STRIPE.PAYMENT
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
+from raw.stripe.payment 
